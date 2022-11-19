fe74b79c9c68b900d5541738649ad0ad77ee3852
EduardoDfs <dueduedudu1>
2022-11-19 19:35:46 -0300

    Colocando os dois projetos no mesmo repositório

---
+++ loja-virtual/src/main/java/com/webstore/entity/PermissaoPessoa.java
@@ -0,0 +1,35 @@
+package com.webstore.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "permissao_pessoa")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class PermissaoPessoa extends Auditavel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pessoa")
+    @JsonIgnore
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "id_permissao")
+    private Permissao permissao;
+
+
+}
